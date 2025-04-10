89ad0ef0a1eae05d5a79263e1d16d2b310b39ed6
glagol <g1agol@proton.me>
2025-04-10 20:27:23 +0300

    added new functions and little bug fixes

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 class Task():
     def new_location(self, count = 5):
@@ -12,6 +13,7 @@ class Task():
 
         random_place_id = []
 
+        print('\nсоздание новых локаций:')
         for i in range(1, count+1):
             json_for_create_new_location = {
                 "location": {
@@ -40,7 +42,6 @@ class Task():
 
                 "language": "French-IN"
             }
-
             result_post = requests.post(post_url, json=json_for_create_new_location)
             if result_post.status_code == 200:
                 place_id = result_post.json().get("place_id")
@@ -57,6 +58,7 @@ class Task():
         with open('id.txt', 'r') as f:
             ids = [line.strip() for line in f.readlines()]
 
+        print('\nданные локаций:')
         for pid in ids[:5]:
             get_resource = '/maps/api/place/get/json'
             get_url = f'{url.rstrip("/")}{get_resource}{key}&place_id={pid}'
@@ -67,6 +69,44 @@ class Task():
                 print(response.json())
             else:
                 print(f"ошибка для ID {pid}: {response.status_code}")
+
+        delete_ids = random_place_id[1::2]
+
+        print("\nудаление локаций:")
+        for pid in delete_ids:
+            # Формируем URL для DELETE запроса
+            delete_resource = "/maps/api/place/delete/json"
+            delete_url = f"{url}{delete_resource}{key}&place_id={pid}"
+            response = requests.delete(delete_url, json={"place_id": pid})
+            if response.status_code == 200:
+                print(f"локация {pid} удалена")
+            else:
+                print(f"ошибка удаления {pid}: {response.status_code}")
+
+        time.sleep(5)
+
+        print("\nпроверка после удаления:")
+        existing_ids = []
+
+        for pid in ids:
+            get_url = f"{url}/maps/api/place/get/json{key}&place_id={pid}"
+            response = requests.get(get_url)
+
+            if response.status_code == 200:
+                existing_ids.append(pid)
+                print(f"локация {pid} существует.")
+            elif response.status_code == 404:
+                print(f"локация {pid} удалена.")
+            else:
+                print(f"ошибка при проверке {pid}: {response.status_code}")
+
+        with open('id2.txt', 'w') as f:
+            for pid in existing_ids:
+                f.write(f"{pid}\n")
+        print(f"\nсоздан файл id2.txt с {len(existing_ids)} активными ID")
+
+
+
 
 test_task = Task()
 test_task.new_location()
